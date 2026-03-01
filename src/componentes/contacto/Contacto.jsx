60bc4fbb892ec418bce60a370e8c34ac1de723fb
Manuel Leguizamon <leguimanu@hotmail.com>
2026-02-28 22:23:29 -0300

    4°Commit: Info fotos y videos actualizados. Footer mejorado

--- src/componentes/contacto/Contacto.jsx
+++ src/componentes/contacto/Contacto.jsx
@@ -1,33 +1,45 @@
 import './Contacto.css'
 import { useState } from 'react';
-import { FaCopy, FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaCopy, FaLinkedin, FaGithub } from "react-icons/fa";
 
 
 export function Contacto() {
 
     const [copiado, setCopiado] = useState(false);
+    const handleCopy = () => {
+        navigator.clipboard.writeText("leguimanu00@gmail.com");
+        setCopiado(true);
+        setTimeout(() => setCopiado(false), 800);
+    };
+
     return(
         <div className='contacto-contenedor'>
             <hr />
             <h2 className='contacto-titulo' id='contacto'>Contacto</h2>
             <div className='contacto-card' id='contacto'>
-                <p className='contactoItem'>
-                    leguimanu00@gmail.com
-                    <FaCopy
-                        onClick={() => {
-                            navigator.clipboard.writeText("leguimanu00@gmail.com");
-                            setCopiado(true);
-                            setTimeout(() => setCopiado(false), 1500);
-                        }}
-                        size={25}
-                        style={{ cursor: "pointer", marginLeft: "5px" }}
-                    />
 
-                    {copiado && <span className="copiado-msj">✔</span>}
+                 <p className='contactoItem'>
+                    <b>leguimanu00@gmail.com</b>
+                    {!copiado ? (
+                        <FaCopy
+                            onClick={handleCopy}
+                            size={25}
+                            style={{ cursor: "pointer", marginLeft: "5px" }}
+                        />
+                    ) : (
+                        <span 
+                            className="copiado-msj"
+                            style={{ marginLeft: "5px", color: "green" }}
+                        >
+                            ✔
+                        </span>
+                    )}
                 </p>                  
+
                     <a href="https://github.com/ManuelLeguizamon" target='_blank' className='contactoItem'>GitHub<FaGithub size={30} /></a>
                     <a href="https://www.linkedin.com/in/manuel-leguizamon-620a01266/" target="_blank" className='contactoItem'>Linkedin<FaLinkedin size={30} /></a>            
             </div>
         </div>
     )
 }
+
